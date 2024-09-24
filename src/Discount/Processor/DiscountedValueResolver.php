71e0290ed2a0dedb4982cb4be8747d8fbff9b478
claude-cp <dev.claude.petrescu@gmail.com>
2024-09-24 16:12:37 +0300

    Add percent or fixed discount value resolver

---
+++ src/Discount/Processor/DiscountedValueResolver.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Discount\Processor;
+
+use App\Discount\Entity\DiscountInterface;
+use App\Discount\Enum\ApplyType;
+
+trait DiscountedValueResolver
+{
+    private static function resolveDiscountedValue(float $referenceValue, DiscountInterface $discount): float
+    {
+        $applyType = $discount->getApplyType();
+
+        switch (true) {
+            case ApplyType::PERCENT === $applyType:
+                return $referenceValue * $discount->getDiscountValue() / 100;
+            case ApplyType::FIXED === $applyType:
+                return (float) max(0.0, $referenceValue - $discount->getDiscountValue() / 100);
+            default:
+                throw new \LogicException(sprintf('Only percent and fixed are supported for now'));
+        }
+    }
+}
